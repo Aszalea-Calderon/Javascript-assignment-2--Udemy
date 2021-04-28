ff768a4e49852c6696a34b309d9e11e9bd063a58
unknown <aszalea_calderon@yahoo.com>
2021-04-28 12:11:23 -0700

    finsihed homework

--- assignment.js
+++ assignment.js
@@ -1,1 +1,16 @@
-const task3Element = document.getElementById('task-3');
+const task3Element = document.getElementById("task-3");
+const task4Element = document.getElementById("task-4");
+task3Element.addEventListener("click", one);
+task4Element.addEventListener("mouseover", con);
+
+function one() {
+  alert("text of your choice");
+}
+
+function peach(name) {
+  alert(`This is from the function called name. The name is ${name}`);
+}
+function concat(s1, s2, s3) {
+  return alert(s1 + s2 + s3);
+}
+concat("i", "heart", "you");
